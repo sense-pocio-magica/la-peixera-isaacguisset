ea878305ef29f94915694e3b5fb2a86b4b2b70cc
isaacguisset <guissetisaac@gmail.com>
2026-03-05 17:36:30 +0100

    afegir propietats de tauler(la peixera)

--- Tasca/Tauler.cs
+++ Tasca/Tauler.cs
@@ -1,6 +1,11 @@
 namespace Tasca;
 
-public class Tauler
-{
-    
+public class Tauler {
+    public List<Animal> Habitants { get; set; } = new List<Animal>();
+    public int Mida { get; set; } = 20;
+
+    public void Inicialitzar()
+    {
+        // encara no he posat res pero ja ho fare
+    }
 }
