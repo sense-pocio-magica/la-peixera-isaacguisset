6c5bc890762cab0baf78a1e2a644bfe9d24fb9c1
isaacguisset <guissetisaac@gmail.com>
2026-03-05 17:49:26 +0100

    propietats tauro + Interactuar i interfície iinteractuable

---
+++ Tasca/Tauro.cs
@@ -0,0 +1,21 @@
+namespace Tasca;
+
+public class Tauro : Animal, IInteractuable
+{
+    public int Vida { get; set; } = 75;
+
+    public override void Moure(int filamax, int columnamax)
+    {
+        X = (X + 1 + filamax) % filamax;
+        Vida--;
+        if (Vida <= 0) Viu = false;
+    }
+
+    public void Interactuar(Animal altre, List<Animal> nousHabitants)
+    {
+        if (altre is Peix)
+        {
+            altre.Viu = false;
+        }
+    }
+}
