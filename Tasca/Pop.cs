3dd3c6a23a983186f0f23513594ed291e69291e7
isaacguisset <guissetisaac@gmail.com>
2026-03-05 17:53:37 +0100

    propietats de pop (afegit a interactuable ja que canvia de sentit al xocar amb algú)

---
+++ Tasca/Pop.cs
@@ -0,0 +1,16 @@
+namespace Tasca;
+
+public class Pop : Animal, IInteractuable
+{
+    private bool sentit = true;  // si es true es cap a la dreta i si es false cap a lesquerra
+
+    public override void Moure(int filamax, int columnamax)
+    {
+       
+    }
+
+    public void Interactuar(Animal altre, List<Animal> nousHabitants)
+    {
+       
+    }
+}
