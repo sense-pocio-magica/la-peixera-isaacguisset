e473fd4b45a99d37fed0c3089d1abffae1124de7
isaacguisset <guissetisaac@gmail.com>
2026-03-05 17:38:08 +0100

    propietats de casella

--- Tasca/Casella.cs
+++ Tasca/Casella.cs
@@ -2,5 +2,11 @@ namespace Tasca;
 
 public class Casella
 {
-    
+    public List<Animal> Habitants { get; set; } = new List<Animal>();
+
+    public void ResoldreXocada()
+    {
+        if (Habitants.Count < 2) return;
+        //aqui he de posar la logica
+    }
 }
