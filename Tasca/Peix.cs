0d0c1480c2a2f02f30ac427e4f1e291e20058fdd
isaacguisset <guissetisaac@gmail.com>
2026-03-05 17:47:59 +0100

    Afegir la interficie a peix per tal de que pugui trobarse o interactuar amb altres animals i dur a terme algunes funcions com reproduir-se

--- Tasca/Peix.cs
+++ Tasca/Peix.cs
@@ -1,5 +1,6 @@
 namespace Tasca;
-public class Peix : Animal
+
+public class Peix : Animal, IInteractuable
 {
     public int DirX { get; set; }
     public int DirY { get; set; }
@@ -7,5 +8,12 @@ public class Peix : Animal
     {
         X = (X + DirX + filamax) % filamax;
         Y = (Y + DirY + columnamax) % columnamax;
+    }
+     public void Interactuar(Animal altre, List<Animal> nousHabitants)
+    {
+        if (altre is Peix && this.Sexe != altre.Sexe)
+        {
+            //encara he de posar la lógica pero de moment vaig a per lo fàcil
+        }
     }
 }
