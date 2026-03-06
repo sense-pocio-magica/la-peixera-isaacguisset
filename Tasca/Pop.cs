c6e12c28173dc2825490d98749141a52ac843c89
isaacguisset <guissetisaac@gmail.com>
2026-03-06 18:05:20 +0100

    cmnt en pop.cs

--- Tasca/Pop.cs
+++ Tasca/Pop.cs
@@ -4,7 +4,7 @@ public class Pop : Animal, IInteractuable
 {
     private bool sentit = true;  // si es true es cap a la dreta i si es false cap a lesquerra
 
-    public override void Moure(int filamax, int columnamax)
+    public override void Moure(int filamax, int columnamax) //potser es massa llarg, he de pensar en simplificar-lo si puc
     {
         if (sentit)
         {
