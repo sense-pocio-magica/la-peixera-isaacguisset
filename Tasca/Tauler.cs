ac00c128e1caae0bcf7e4b8264af2558137f7ff2
isaacguisset <guissetisaac@gmail.com>
2026-03-06 18:11:50 +0100

    afegir pops tortugues i taurons en el tauler

--- Tasca/Tauler.cs
+++ Tasca/Tauler.cs
@@ -14,6 +14,20 @@ public class Tauler
             Habitants.Add(new Peix(r.Next(Mida), r.Next(Mida), r.Next(-1, 2), r.Next(-1, 2), 'M'));
             Habitants.Add(new Peix(r.Next(Mida), r.Next(Mida), r.Next(-1, 2), r.Next(-1, 2), 'F'));
         }
+        for (int i = 0; i < 15; i++)
+        {
+            Habitants.Add(new Pop { X = 0, Y = r.Next(Mida) });
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Habitants.Add(new Tauro { X = r.Next(Mida), Y = r.Next(Mida), Sexe = 'M' });
+            Habitants.Add(new Tauro { X = r.Next(Mida), Y = r.Next(Mida), Sexe = 'F' });
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            Habitants.Add(new Tortuga { X = r.Next(Mida), Y = r.Next(Mida), Sexe = 'M', DirX = 1, DirY = 0 });
+            Habitants.Add(new Tortuga { X = r.Next(Mida), Y = r.Next(Mida), Sexe = 'F', DirX = 0, DirY = 1 });
+        }
     }
     public void FerForaElsMorts()
     {
