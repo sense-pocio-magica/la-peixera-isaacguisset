4a391a1068fb280b5ef78801130d230988887dca
isaacguisset <guissetisaac@gmail.com>
2026-03-06 18:01:48 +0100

    metode resoldre xocada a casella.cs

--- Tasca/Casella.cs
+++ Tasca/Casella.cs
@@ -7,6 +7,20 @@ public class Casella
     public void ResoldreXocada()
     {
         if (Habitants.Count < 2) return;
-        //aqui he de posar la logica
+
+        List<Animal> nous = new List<Animal>();
+        for (int i = 0; i < Habitants.Count - 1; i++)
+        {
+            for (int j = i + 1; j < Habitants.Count; j++)
+            {
+                if (Habitants[i].Viu && Habitants[j].Viu)
+                {
+                    if (Habitants[i] is IInteractuable inter)
+                        inter.Interactuar(Habitants[j], nous);
+                }
+            }
+        }
+        Habitants.AddRange(nous);
     }
+
 }
