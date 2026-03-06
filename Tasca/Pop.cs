af1d821a40c6e0bd35bcf9d28149d11a0d2bdf05
isaacguisset <guissetisaac@gmail.com>
2026-03-06 11:29:46 +0100

    logica de moviment de pop

--- Tasca/Pop.cs
+++ Tasca/Pop.cs
@@ -6,11 +6,52 @@ public class Pop : Animal, IInteractuable
 
     public override void Moure(int filamax, int columnamax)
     {
-       
+        if (sentit)
+        {
+            if (X == 0 && Y < columnamax - 1) // mentres esta adalt i no arriba al costat de dreta es va moveent cap a la dreta
+            {
+                Y++;
+            }
+            else if (Y == columnamax - 1 && X < filamax - 1) //quan hi arriba baixa
+            {
+                X++;
+            }
+            else if (X == filamax - 1 && Y > 0) // quan esta abaix ja fins que no arriba a lesquerra es va movent
+            {
+                Y--;
+            }
+            else if (Y == 0 && X > 0)//fins que no arriba adalt es va movent
+            {
+                X--;
+            }
+        }
+        else // basicament lo mateix al reves
+        {
+            if (X == 0 && Y > 0)
+            {
+                Y--;
+            }
+            else if (Y == 0 && X < filamax - 1)
+            {
+                X++;
+            }
+            else if (X == filamax - 1 && Y < columnamax - 1)
+            {
+                Y++;
+            }
+            else if (Y == columnamax - 1 && X > 0)
+            {
+                X--;
+            }
+        }
     }
 
     public void Interactuar(Animal altre, List<Animal> nousHabitants)
     {
-       
+        // si troben un altre pop van cap al sentit que no sigui el d ara
+        if (altre is Pop)
+        {
+            this.sentit = !this.sentit;
+        }
     }
 }
