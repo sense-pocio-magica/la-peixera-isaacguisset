e338a2c97ac25f7136655cefd45fb2d0e280a13e
isaacguisset <guissetisaac@gmail.com>
2026-03-06 11:23:06 +0100

    afegir tortuga.cs i lógica (falta revisar...)

--- Tasca/Tortuga.cs
+++ Tasca/Tortuga.cs
@@ -2,17 +2,21 @@ namespace Tasca;
 
 public class Tortuga : Animal, IInteractuable
 {
+    
     public int DirX { get; set; }
     public int DirY { get; set; }
-
     public override void Moure(int filamax, int columnamax)
     {
         X = (X + DirX + filamax) % filamax;
+
         Y = (Y + DirY + columnamax) % columnamax;
     }
 
+
+
     public void Interactuar(Animal altre, List<Animal> nousHabitants)
     {
+
         if (altre is Tortuga && this.Sexe == altre.Sexe)
         {
             this.Viu = false;
@@ -21,9 +25,10 @@ public class Tortuga : Animal, IInteractuable
         else if (altre is Tortuga && this.Sexe != altre.Sexe)
         {
             Random r = new Random();
-            nousHabitants.Add(new Tortuga { 
-                X = this.X, 
-                Y = this.Y, 
+            nousHabitants.Add(new Tortuga
+            {
+                X = this.X,
+                Y = this.Y,
                 Sexe = r.Next(0, 2) == 0 ? 'M' : 'F',
                 DirX = r.Next(-1, 2),
                 DirY = r.Next(-1, 2)
