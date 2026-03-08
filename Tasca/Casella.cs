021b721a3678366a3e1e399d5cdbb6437cf80021
isaacguisset <guissetisaac@gmail.com>
2026-03-08 19:01:53 +0100

    Fix error es maten mes d'una vegada a casella resoldrexocada

--- Tasca/Casella.cs
+++ Tasca/Casella.cs
@@ -9,16 +9,17 @@ public class Casella
         if (Habitants.Count < 2) return;
 
         List<Animal> nous = new List<Animal>();
+
         for (int i = 0; i < Habitants.Count - 1; i++)
         {
-            for (int j = i + 1; j < Habitants.Count; j++)
+      for (int j = i + 1; j < Habitants.Count; j++)
             {
-                if (Habitants[i].Viu && Habitants[j].Viu)
+                  if (Habitants[i].Viu && Habitants[j].Viu)
                 {
                     if (Habitants[i] is IInteractuable inter1)
                         inter1.Interactuar(Habitants[j], nous);
 
-                    if (Habitants[j] is IInteractuable inter2)
+                if (Habitants[j].Viu && Habitants[j] is IInteractuable inter2)
                         inter2.Interactuar(Habitants[i], nous);
                 }
             }
