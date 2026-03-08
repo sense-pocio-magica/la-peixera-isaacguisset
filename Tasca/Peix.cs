75ff7cbfbc0562b00bb184fcb4fadb404a0ccc43
isaacguisset <guissetisaac@gmail.com>
2026-03-08 16:26:05 +0100

    fer funció de reproduir i aplicar la interficie reproducible a peix

--- Tasca/Peix.cs
+++ Tasca/Peix.cs
@@ -1,6 +1,6 @@
 namespace Tasca;
 
-public class Peix : Animal, IInteractuable,IDesplaçable
+public class Peix : Animal, IInteractuable,IDesplaçable,IReproducible
 {
     public int DirX { get; set; }
     public int DirY { get; set; }
@@ -18,34 +18,35 @@ public class Peix : Animal, IInteractuable,IDesplaçable
         X = (X + DirX + filamax) % filamax;
         Y = (Y + DirY + columnamax) % columnamax;
     }
-public void Interactuar(Animal altre, List<Animal> nousHabitants)
-{
-    if (altre is Peix p)
-
-
+    public void Interactuar(Animal altre, List<Animal> nousHabitants)
     {
-        if (this.Sexe != p.Sexe)//es reprodueixen
+        if (altre is Peix p)
+
+
         {
-            int novaDirX = 1;
-            int novaDirY = 1;
-            if (this.DirX != 0)
+            if (this.Sexe != p.Sexe)//es reprodueixen
             {
-                novaDirX = -this.DirX;
+
+                nousHabitants.Add(Reproduir(this));
             }
-            if (this.DirY != 0)
+            else//es maten entre ells o entre elles :/
             {
-                novaDirY = -this.DirY;
+                this.Morir();
+                p.Morir();
             }
+        }
+    }
+ public Animal Reproduir(Animal altre)
+    {
+        if (altre is Peix p && this.Sexe != p.Sexe)
+        {
+            int novaDirX = this.DirX != 0 ? -this.DirX : 1;
+            int novaDirY = this.DirY != 0 ? -this.DirY : 1;
 
             char nouSexe = Aleat.Random.Next(2) == 0 ? 'F' : 'M';
 
-            nousHabitants.Add(new Peix(this.X, this.Y, novaDirX, novaDirY, nouSexe));
+            return new Peix(this.X, this.Y, novaDirX, novaDirY, nouSexe);
         }
-        else//es maten entre ells o entre elles :/
-        {
-                this.Morir();
-                p.Morir();
-        }
+        return null;
     }
-}
 }
